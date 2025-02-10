6badceb7d80a33d05be025de6cfc67b4a53fa460
Gnanasekaran Sakthivel (Edison) <gnanasekaran.sakthivel@gmail.com>
2025-02-09 22:30:24 -0500

    sequelize ssl connection

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,23 +1,28 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { Sequelize } from "sequelize";
+import { Sequelize, Options } from "sequelize";
 
 // TODO: Create sequelize connection
+const sequelize_options: Options = {
+  host: "localhost",
+  dialect: "postgres",
+  dialectOptions: {
+    decimalNumbers: true,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Important for self-signed certificates
+    },
+  },
+};
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+  ? new Sequelize(process.env.DB_URL, sequelize_options)
   : new Sequelize(
       process.env.DB_NAME || "",
       process.env.DB_USER || "",
       process.env.DB_PASSWORD,
-      {
-        host: "localhost",
-        dialect: "postgres",
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      }
+      sequelize_options
     );
 
 export default sequelize;
